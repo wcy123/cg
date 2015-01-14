6d9e5378d3f82084f4da122c761d799f0f2bf1fb
wcy123 <wcy123@gmail.com>
2015-01-14 10:45:00 +0800

    fib example

--- cg_fib.c
+++ cg_fib.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "cg.h"
-uintptr_t fib(uintptr_t limit)
+uintptr_t fib(uintptr_t limit, uintptr_t undef)
 {
     uintptr_t a=1;
     uintptr_t b=1;
-    uintptr_t t=0;
-    uintptr_t flag = 0;
-    do {
-        t = a+b;
-        a = b;
-        b = t; 
-        flag = cg_yield(__builtin_frame_address(0), t);
-    }while(flag);
-    return t;
+    uintptr_t t= a + b;
+    uintptr_t count = 0;
+    for(t = a+b; t < limit; t = a+b){
+        count = cg_yield(__builtin_frame_address(0), t);
+        a = b; b = t;
+        fprintf(stderr,__FILE__ ":%d:[%s] %ld => %ld\n", __LINE__, __FUNCTION__
+                ,count, t);
+    }
+    return undef;
 }
 int main(int argc, char *argv[])
 {
-    cg_t * co = cg_create((void*)fib,0,0);
+    const uintptr_t undef = 1976;
+    cg_t * co = cg_create((void*)fib,100,undef);
     uintptr_t x = 0;
-    for(x = cg_invoke(co, x < 100);
-        !cg_is_done(co);
-        x = cg_invoke(co, x < 100)){
-        fprintf(stderr,__FILE__ ":%d:[%s] %ld \n",__LINE__, __FUNCTION__
-                ,x);
+    uintptr_t count = 0;
+    for(x = cg_invoke(co, count++);
+        x != undef;
+        x = cg_invoke(co, count++)){
+        fprintf(stderr,__FILE__ ":%d:[%s] %ld \n",__LINE__, __FUNCTION__,x);
 
     }
+    assert(cg_is_done(co));
     return 0;
 }
 
